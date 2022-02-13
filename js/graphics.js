ca720a8764e438e501e18bb221f49a47189a6739
p98angel <p98angel@gmail.com>
2022-02-13 16:22:21 -0600

    v1.3 add info, graphics

--- js/graphics.js
+++ js/graphics.js
@@ -19,10 +19,10 @@ function tbody (_id, _type, _labels, _label_max, _data, _colors, _hover_off) {
 let _id = 'g_presentacion';
 // bar, bubble, doughnut, pie, line, polarArea
 let _type = 'doughnut' ;
-let _labels = ['Femenino', 'Masculino'];
+let _labels = ['Femenino', 'Masculino', 'Valmiki'];
 let _label_max = 'Sexo';
-let _data = [75, 25];
-let _colors = ['pink', 'blue'];
+let _data = [75, 25, 1];
+let _colors = ['pink', 'blue', 'purple'];
 let _hover_off = 3;
 // change only
 tbody(_id, _type, _labels, _label_max, _data, _colors, _hover_off);
@@ -33,9 +33,20 @@ _id = 'g_muestreo';
 // bar, bubble, doughnut, pie, line, polarArea
 _type = 'polarArea' ;
 _labels = ['Azul', 'Naranja', 'Rosa', 'Verde', 'Gris', 'Rojo', 'Negro', 'Marron', 'Morado'];
-_label_max = 'Sexo';
+_label_max = 'Colores';
 _data = [100, 150, 200,250, 300, 350, 400, 450, 500];
 _colors = ['blue', 'orange', 'pink', 'green', 'gray', 'red', 'black', 'brown', 'purple'];
+_hover_off = 3;
+// change only
+tbody(_id, _type, _labels, _label_max, _data, _colors, _hover_off);
+
+_id = 'g-muestreo-persona';
+// bar, bubble, doughnut, pie, line, polarArea
+_type = 'pie' ;
+_labels = ['Voleibol', 'Futbol','Futbol americano','Basquetbol','Beisbol','Esgrima','Gimnasia','Natacion'];
+_label_max = 'Colores';
+_data = [8,25,13,15,27,2,3,7];
+_colors = ['blue', 'orange','pink','green','red','black','brown','purple'];
 _hover_off = 3;
 // change only
 tbody(_id, _type, _labels, _label_max, _data, _colors, _hover_off);
